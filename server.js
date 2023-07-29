3986513fb5358032bb49e89a4bf78ab8f274dd5a
rizkisahat996 <rizkisitumeang77@gmail.com>
2023-07-29 21:11:08 +0700

    oke

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = 'localhost';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
